0056ac7d1d16712e272c7e760e006ad2305b80ac
f.janmohammadi <farshadjanmohammadi@gmail.com>
2024-12-13 15:56:45 +0330

    add Persist

--- src/store/useCustomerStore/index.ts
+++ src/store/useCustomerStore/index.ts
@@ -1,9 +1,21 @@
-import {create} from "zustand";
+import {create} from "zustand"
+import {persist} from "zustand/middleware";
 
-const useCustomerStore = create<ICustomerStore>((set)=> ({
-    customers: [],
-    setCustomers: (value) => set({customers: value}),
-    removeCustomers: ()=> set({customers: []}),
-}))
+const useCustomerStore = create(
+    persist(
+        (set) => ({
+            customers: [],
+            setCustomers: (value:  {
+                id: number;
+                name: string;
+            }[]) => set({ customers: value }),
+            removeCustomers: () => set({ customers: [] }),
+        }),
+        {
+            name: "customer-storage",
+
+        }
+    )
+);
 
 export default useCustomerStore;
